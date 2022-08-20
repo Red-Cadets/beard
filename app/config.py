55cd15dd986a9c5c8fce6d89a251b175b0ebba84
b1n4r9 <vam6612@mail.ru>
2022-08-20 18:27:14 +0000

    (api): config load from file

--- app/config.py
+++ app/config.py
@@ -3,20 +3,20 @@ from pydantic import BaseSettings
 from functools import lru_cache
 
 
-# TODO: брать конфиг из файла в зависимости от переменной dev
-# TODO: исправить временные значения на стандартные для продакшн сборки
+def get_debug():
+    return bool(os.getenv('DEBUG', False))
+
+
 class Config(BaseSettings):
-    SCOREBOARD: str = os.getenv('SCOREBOARD', 'http://127.0.0.1:8090')
-    TEAM: str = os.getenv('TEAM', 'Red Cadets')
-    TYPE: str = os.getenv('TYPE', 'forcad')
-    BOT_URL: str = os.getenv('BOT_URL', 'https://bot.example.com/key')
-    MONGO_HOST: str = os.getenv('MONGO_HOST', '127.0.0.1')
-    MONGO_PORT: int = int(os.getenv('MONGO_PORT', '27017'))
-    MONGO_USER: str = os.getenv('MONGO_USER', 'beard')
-    MONGO_PASS: str = os.getenv('MONGO_PASS', 'beard')
-    ROUND_TIME: int = int(os.getenv('ROUND_TIME', '120'))
-    EXTEND_ROUND: int = int(os.getenv('EXTEND_ROUND', '50'))
-    DEBUG: bool = bool(os.getenv('DEBUG', 'true'))
+    BOT_URL: str = ""
+    MONGO_HOST: str = '127.0.0.1'
+    MONGO_PORT: int = 27017
+    MONGO_USER: str = 'beard'
+    MONGO_PASS: str = 'beard'
+    DEBUG: bool = get_debug()
+
+    class Config:
+        env_file = ".dev.env" if get_debug() else ".env"
 
 
 @lru_cache()
